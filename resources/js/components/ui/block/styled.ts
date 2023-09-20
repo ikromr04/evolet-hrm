c3adcfbc616a8e2a478a54a4bfac30d28c6d401b
ikromr04 <ikromr04@gmail.com>
2023-09-20 11:52:25 +0500

    Adds shadow to Block component

--- resources/js/components/ui/block/styled.ts
+++ resources/js/components/ui/block/styled.ts
@@ -4,4 +4,5 @@ export const StyledBlock = styled.div`
   background-color: ${({ theme }) => theme.color.baseWhite};
   border: 1px solid #dce5ef;
   border-radius: 8px;
+  box-shadow: 0 0px 4px rgba(0,0,0,0.05);
 `;
