6bedc84e2bca1d86d7fafb3f37aba2ebc9a78789
ikromr04 <ikromr04@gmail.com>
2023-10-04 00:49:50 +0500

    Refactors Employeeeducations component

--- resources/js/components/pages/employee-page/employee-educations/delete-education/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import Modal from '../../../../ui/modal/modal';
-
-export const StyledModal = styled(Modal)`
-  margin-left: auto;
-`;
--- resources/js/components/ui/box/box.tsx
+++ resources/js/components/ui/box/box.tsx
@@ -4,17 +4,20 @@ import { StyledBox } from './styled';
 type BoxProps = PropsWithChildren<{
   className?: string;
   tagName?: string;
+  [rest: string]: any
 }>;
 
 function Box({
   children,
   className,
   tagName,
+  ...rest
 }: BoxProps): JSX.Element {
   return (
     <StyledBox
       className={className}
       as={tagName}
+      {...rest}
     >
       {children}
     </StyledBox>
